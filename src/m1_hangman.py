88d0b7671d671d3db4b143c111b47075a55f062a
hughesm1 <hughesm1@rose-hulman.edu>
2019-01-24 11:40:00 -0500

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -8,3 +8,62 @@ Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
 
 ####### Do NOT attempt this assignment before class! #######
 
+import random
+
+
+def main():
+    anything = []
+    x = rand_word()
+    initial_dashes(x)
+    for k in range(3):
+        true_false, kval = guess(x)
+        anything = anything + [kval + 1]
+        dashes(x, kval, anything)
+
+
+def rand_word():
+    with open('words.txt') as f:
+        f.readline()
+        string = f.read()
+        words = string.split()
+    r = random.randrange(0, len(words))
+    word = words[r]
+    print(word)
+    return word
+
+# def word_length():
+#     variable = input('Minimum length of word')
+#     if variable
+
+
+def guess(word):
+    guess1 = input('What letter do you want to try?')
+    for k in range(len(word)):
+        if word[k] == guess1:
+            print(word[k])
+            return True, k
+
+
+
+def dashes(word, kval, anything):
+    dash = []
+    for k in range(len(word)):
+        dash = dash + ['-']
+    for u in range(len(anything) + 1):
+        dash[kval] = word[kval]
+    print(kval)
+    print(anything)
+    print(dash)
+
+
+def initial_dashes(word):
+    dash = []
+    for k in range(len(word)):
+        dash = dash + ['-']
+
+
+
+
+
+
+main()
